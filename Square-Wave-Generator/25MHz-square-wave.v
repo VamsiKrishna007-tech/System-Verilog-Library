98fa2101986bd3a593fba845213a6c072f3a6f45
Vamsi Krishna <electricals.vamsikrishna@gmail.com>
2026-06-25 16:12:13 +0530

    Update 25MHz-square-wave.v

--- Square-Wave-Generator/25MHz-square-wave.v
+++ Square-Wave-Generator/25MHz-square-wave.v
@@ -1,11 +1,34 @@
-`timescale 1ps/1ps
-module tb();
-reg clk;
 
-initial begin 
-clk = 1'b0;
+`timescale 1ns / 1ps
+
+module tb();
+reg clk = 0; 
+reg clk40 = 0;
+
+always #5 clk = ~clk;   //Reference Clock 100MHz
+  
+task clkgen(input real ton, input real toff);  
+  while(1) 
+  begin
+  clk40 = 1;
+  #ton;
+  clk40 = 0;
+  #toff;
+  end
+endtask
+
+task calc (input real freq_hz, input real duty_cycle, output real ton, output real toff);
+ton = (1.0 / freq_hz) * duty_cycle * 1000_000;
+toff =  (1000_000 / freq_hz) - ton;
+endtask
+
+initial begin
+calc(25, 0.4, ton, toff);
+clkgen(ton, toff);
 end
 
-always #20000 clk = ~clk; # 25MHz = 40000ps 
-
+  initial begin
+    #200;
+    $finish();
+  end
 endmodule
