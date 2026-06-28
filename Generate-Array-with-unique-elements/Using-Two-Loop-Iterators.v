dea7c8fb2a56bce1d7e25dd252361f0b3bc3cb03
Vamsi Krishna <electricals.vamsikrishna@gmail.com>
2026-06-28 16:27:37 +0530

    Update Using-Two-Loop-Iterators.v

--- Generate-Array-with-unique-elements/Using-Two-Loop-Iterators.v
+++ Generate-Array-with-unique-elements/Using-Two-Loop-Iterators.v
@@ -1,1 +1,22 @@
+program unique_array;
+  class C2;
+    rand bit [4:0] A [];
+    //constrain A's size between 10 to 15
+    constraint c1 {
+      A.size inside {[10:15]};}
+    //use iterators and conditions to generate unique elements
+    constraint c2 {
+      foreach(A[i])
+        foreach(A[j])
+          if(i!=j)
+            A[i] != A[j];}
+  endclass
 
+  initial begin
+    C2 C2_h = new();
+    repeat(10) begin
+      if(C2_h.randomize())
+        $display("A is %p", C2_h.A);
+    end
+  end
+endprogram
