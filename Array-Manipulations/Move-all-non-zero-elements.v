125dd0d3828f4d5ed9a4cd0a9d86359214fb22d4
Vamsi Krishna <electricals.vamsikrishna@gmail.com>
2026-06-28 15:20:14 +0530

    Create Move-all-non-zero-elements.v

---
+++ Array-Manipulations/Move-all-non-zero-elements.v
@@ -0,0 +1,7 @@
+Given a dynamic array of integers, move all non-zero elements to the left while maintaining their relative order. All zero elements should be moved to the right.
+
+Rules:
+
+Move all non-zero elements to the left while keeping their order.
+
+Push all zeros to the right (in-place, no extra arrays).
