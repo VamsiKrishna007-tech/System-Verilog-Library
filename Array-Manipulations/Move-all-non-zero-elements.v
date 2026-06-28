39b34a0e1471a0fd6ae89b062277cdcd66271eac
Vamsi Krishna <electricals.vamsikrishna@gmail.com>
2026-06-28 15:23:11 +0530

    Update Move-all-non-zero-elements.v

--- Array-Manipulations/Move-all-non-zero-elements.v
+++ Array-Manipulations/Move-all-non-zero-elements.v
@@ -1,7 +1,60 @@
-Given a dynamic array of integers, move all non-zero elements to the left while maintaining their relative order. All zero elements should be moved to the right.
+module tb;
+    task automatic run_test(
+        input string test_name,
+        ref int input_arr[],
+        ref int expected[]
+    );
 
-Rules:
+        int temp, j=0;
+        for (int i=0; i< input_arr.size(); i++) begin
+          if (input_arr[i] != 0) begin
+            temp = input_arr[i];
+            input_arr[i] = input_arr[j];
+            input_arr[j] = temp;
+            j++;
+          end
+        end    
 
-Move all non-zero elements to the left while keeping their order.
+        if (input_arr.size() != expected.size()) begin
+            $display("[FAIL] %s : Size mismatch", test_name);
+            return;
+        end
 
-Push all zeros to the right (in-place, no extra arrays).
+        foreach (expected[i]) begin
+            if (input_arr[i] != expected[i]) begin
+                $display("[FAIL] %s", test_name);
+                $display("Expected = %p", expected);
+                $display("Got      = %p", input_arr);
+                return;
+            end
+        end
+        $display("[PASS] %s", test_name);
+    endtask
+
+    initial begin
+        int arr1[] = '{1,9,8,4,0,0,2,7,0,6,0};
+        int exp1[] = '{1,9,8,4,2,7,6,0,0,0,0};
+
+        int arr2[] = '{0,0,0,1,2,3};
+        int exp2[] = '{1,2,3,0,0,0};
+
+        int arr3[] = '{5};
+        int exp3[] = '{5};
+
+        $display("input_arr : %p", arr1);
+        $display("expected : %p", exp1);
+
+        run_test("Basic Example - 1", arr1, exp1);
+
+
+        $display("input_arr : %p", arr2);
+        $display("expected : %p", exp2);
+
+        run_test("Leading Zeros - 2", arr2, exp2);
+
+        $display("input_arr : %p", arr3);
+        $display("expected : %p", exp3);
+        run_test("Single Element - 3", arr3, exp3);
+        
+    end
+endmodule
